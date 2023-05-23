f78c00d1ab1a4c66617c988f323c53f83f7c3d91
Mario Gilezan <mariogilezan10@gmail.com>
2023-05-23 17:56:49 +0200

    add data for component list

---
+++ data/componentListItems.js
@@ -0,0 +1,34 @@
+export const componentsListItems = [
+  {
+    name: 'CPU',
+    href: 'cpu',
+  },
+  {
+    name: 'CPU Cooler',
+    href: 'cpuCooler',
+  },
+  {
+    name: 'Motherboard',
+    href: 'motherboard',
+  },
+  {
+    name: 'Memory',
+    href: 'memory',
+  },
+  {
+    name: 'Storage',
+    href: 'storage',
+  },
+  {
+    name: 'Video Card',
+    href: 'gpu',
+  },
+  {
+    name: 'Case',
+    href: 'case',
+  },
+  {
+    name: 'PSU',
+    href: 'psu',
+  },
+];
