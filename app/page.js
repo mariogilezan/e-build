5c8d4415d8c5adad821039bb7b9b07c24401459a
Mario Gilezan <mariogilezan10@gmail.com>
2023-05-23 18:32:16 +0200

    add markup for components list selection

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 'use client';
 
+import { componentsListItems } from '@/data/componentListItems';
+import Link from 'next/link';
 import { useState } from 'react';
 
 export default function Home() {
   const [totalCost, setTotalCost] = useState(0);
 
   return (
-    <main className='container mx-auto flex min-h-screen flex-col items-center justify-center gap-10 px-4 py-8'>
+    <main className='container mx-auto flex min-h-screen flex-col items-center gap-10 px-4 py-8'>
       <header className='flex flex-col items-center text-center gap-4'>
         <h1 className='text-4xl font-bold'>
           <span className='text-red-600'>E</span>Build
@@ -16,54 +18,22 @@ export default function Home() {
         </p>
       </header>
 
-      <table className='table-auto w-full border-collapse'>
-        <thead className='text-left hidden sm:table-header-group'>
-          <tr className='border-b border-slate-500 grid grid-cols-4'>
-            <th className='p-4'>Component</th>
-            <th className='p-4 col-span-2'>Selection</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className='border-b border-slate-500'>
-            <td className='p-4'>CPU</td>
-            <td className='p-4'>Select CPU</td>
-          </tr>
-          <tr className='border-b border-slate-500'>
-            <td className='p-4'>CPU Cooler</td>
-            <td className='p-4'>Select CPU Cooler</td>
-          </tr>
-          <tr className='border-b border-slate-500'>
-            <td className='p-4'>Motherboard</td>
-            <td className='p-4'>Select Motherboard</td>
-          </tr>
-          <tr className='border-b border-slate-500'>
-            <td className='p-4'>Memory</td>
-            <td className='p-4'>Select Memory</td>
-          </tr>
-          <tr className='border-b border-slate-500'>
-            <td className='p-4'>Storage</td>
-            <td className='p-4'>Select Storage</td>
-          </tr>
-          <tr className='border-b border-slate-500'>
-            <td className='p-4'>Video Card</td>
-            <td className='p-4'>Select Video Card</td>
-          </tr>
-          <tr className='border-b border-slate-500'>
-            <td className='p-4'>Case</td>
-            <td className='p-4'>Select Case</td>
-          </tr>
-          <tr className='border-b border-slate-500'>
-            <td className='p-4'>Power Supply</td>
-            <td className='p-4'>Select Power Supply</td>
-          </tr>
-          {totalCost > 0 && (
-            <tr className='border-b border-slate-500'>
-              <td className='p-4'>Total:</td>
-              <td className='p-4'>{`$${totalCost}`}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <section className='w-full border-2 border-slate-600 p-4 rounded flex flex-col gap-4 text-sm sm:text-base'>
+        {componentsListItems.map((component) => (
+          <div
+            key={component.name}
+            className='grid grid-cols-1 gap-2 items-center'
+          >
+            <div>{component.name}</div>
+            <Link
+              href={`/${component.href}`}
+              className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
+            >
+              Select {component.name}
+            </Link>
+          </div>
+        ))}
+      </section>
     </main>
   );
 }
