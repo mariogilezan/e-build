2e1bd76382e5f42436d981aa0ce274a7c0f59299
Mario Gilezan <mariogilezan10@gmail.com>
2023-05-31 00:32:02 +0200

    restructure components list markup and use data from redux global state

--- app/page.js
+++ app/page.js
@@ -29,7 +29,9 @@ export default function Home() {
       <section className='w-full sm:w-4/5 md:w-3/4 lg:w-3/5 border-2 border-slate-600 p-4 rounded flex flex-col gap-4 text-sm sm:text-base'>
         {/* CPU */}
         <div className='grid grid-cols-1 gap-2 items-center'>
-          <div>CPU</div>
+          <h2 className='font-bold md:text-xl'>
+            <span className='text-red-600'>C</span>PU
+          </h2>
           {cpu !== null ? (
             <div className='flex items-center justify-between gap-4'>
               <Link
@@ -53,73 +55,185 @@ export default function Home() {
         </div>
         {/* CPU Cooler */}
         <div className='grid grid-cols-1 gap-2 items-center'>
-          <div>CPU Cooler</div>
-          <Link
-            href='/cpuCooler'
-            className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
-          >
-            Select CPU Cooler
-          </Link>
+          <h2 className='font-bold md:text-xl'>
+            <span className='text-red-600'>C</span>PU Cooler
+          </h2>
+          {cpuCooler !== null ? (
+            <div className='flex items-center justify-between gap-4'>
+              <Link
+                href='/cpuCooler'
+                className='truncate font-semibold hover:text-cyan-600 hover:underline'
+              >
+                {cpuCooler.name} - {cpuCooler.price}
+              </Link>
+              <button className='text-gray-400 hover:text-red-600 font-bold text-lg w-4 h-4 flex items-center justify-center'>
+                &times;
+              </button>
+            </div>
+          ) : (
+            <Link
+              href='/cpuCooler'
+              className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
+            >
+              Select CPU Cooler
+            </Link>
+          )}
         </div>
         {/* Motherboard */}
         <div className='grid grid-cols-1 gap-2 items-center'>
-          <div>Motherboard</div>
-          <Link
-            href='/motherboard'
-            className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
-          >
-            Select Motherboard
-          </Link>
+          <h2 className='font-bold md:text-xl'>
+            <span className='text-red-600'>M</span>otherboard
+          </h2>
+          {motherboard !== null ? (
+            <div className='flex items-center justify-between gap-4'>
+              <Link
+                href='/motherboard'
+                className='truncate font-semibold hover:text-cyan-600 hover:underline'
+              >
+                {motherboard.name} - {motherboard.price}
+              </Link>
+              <button className='text-gray-400 hover:text-red-600 font-bold text-lg w-4 h-4 flex items-center justify-center'>
+                &times;
+              </button>
+            </div>
+          ) : (
+            <Link
+              href='/motherboard'
+              className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
+            >
+              Select Motherboard
+            </Link>
+          )}
         </div>
         {/* Memory */}
         <div className='grid grid-cols-1 gap-2 items-center'>
-          <div>Memory</div>
-          <Link
-            href='/memory'
-            className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
-          >
-            Select Memory
-          </Link>
+          <h2 className='font-bold md:text-xl'>
+            <span className='text-red-600'>M</span>emory
+          </h2>
+          {memory !== null ? (
+            <div className='flex items-center justify-between gap-4'>
+              <Link
+                href='/memory'
+                className='truncate font-semibold hover:text-cyan-600 hover:underline'
+              >
+                {memory.name} - {memory.price}
+              </Link>
+              <button className='text-gray-400 hover:text-red-600 font-bold text-lg w-4 h-4 flex items-center justify-center'>
+                &times;
+              </button>
+            </div>
+          ) : (
+            <Link
+              href='/memory'
+              className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
+            >
+              Select Memory
+            </Link>
+          )}
         </div>
         {/* Storage */}
         <div className='grid grid-cols-1 gap-2 items-center'>
-          <div>Storage</div>
-          <Link
-            href='/storage'
-            className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
-          >
-            Select Storage
-          </Link>
+          <h2 className='font-bold md:text-xl'>
+            <span className='text-red-600'>S</span>torage
+          </h2>
+          {storage !== null ? (
+            <div className='flex items-center justify-between gap-4'>
+              <Link
+                href='/storage'
+                className='truncate font-semibold hover:text-cyan-600 hover:underline'
+              >
+                {storage.name} - {storage.price}
+              </Link>
+              <button className='text-gray-400 hover:text-red-600 font-bold text-lg w-4 h-4 flex items-center justify-center'>
+                &times;
+              </button>
+            </div>
+          ) : (
+            <Link
+              href='/storage'
+              className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
+            >
+              Select Storage
+            </Link>
+          )}
         </div>
         {/* Video Card */}
         <div className='grid grid-cols-1 gap-2 items-center'>
-          <div>Video Card</div>
-          <Link
-            href='/gpu'
-            className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
-          >
-            Select Video Card
-          </Link>
+          <h2 className='font-bold md:text-xl'>
+            <span className='text-red-600'>V</span>ideo Card
+          </h2>
+          {gpu !== null ? (
+            <div className='flex items-center justify-between gap-4'>
+              <Link
+                href='/gpu'
+                className='truncate font-semibold hover:text-cyan-600 hover:underline'
+              >
+                {`${gpu.name} (${gpu.chipset})`} - {gpu.price}
+              </Link>
+              <button className='text-gray-400 hover:text-red-600 font-bold text-lg w-4 h-4 flex items-center justify-center'>
+                &times;
+              </button>
+            </div>
+          ) : (
+            <Link
+              href='/gpu'
+              className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
+            >
+              Select Video Card
+            </Link>
+          )}
         </div>
         {/* Case */}
         <div className='grid grid-cols-1 gap-2 items-center'>
-          <div>Case</div>
-          <Link
-            href='/case'
-            className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
-          >
-            Select Case
-          </Link>
+          <h2 className='font-bold md:text-xl'>
+            <span className='text-red-600'>C</span>ase
+          </h2>
+          {caseData !== null ? (
+            <div className='flex items-center justify-between gap-4'>
+              <Link
+                href='/case'
+                className='truncate font-semibold hover:text-cyan-600 hover:underline'
+              >
+                {caseData.name} - {caseData.price}
+              </Link>
+              <button className='text-gray-400 hover:text-red-600 font-bold text-lg w-4 h-4 flex items-center justify-center'>
+                &times;
+              </button>
+            </div>
+          ) : (
+            <Link
+              href='/case'
+              className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
+            >
+              Select Case
+            </Link>
+          )}
         </div>
         {/* PSU */}
         <div className='grid grid-cols-1 gap-2 items-center'>
-          <div>PSU</div>
-          <Link
-            href='/psu'
-            className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
-          >
-            Select PSU
-          </Link>
+          <h2 className='font-bold md:text-xl'>
+            <span className='text-red-600'>P</span>ower Supply
+          </h2>
+          {psu !== null ? (
+            <div className='flex items-center justify-between gap-4'>
+              <Link
+                href='/psu'
+                className='truncate font-semibold hover:text-cyan-600 hover:underline'
+              >
+                {`${psu.name} (${psu.wattage})`} - {psu.price}
+              </Link>
+              <button className='text-gray-400 hover:text-red-600 font-bold text-lg w-4 h-4 flex items-center justify-center'>
+                &times;
+              </button>
+            </div>
+          ) : (
+            <Link
+              href='/psu'
+              className='bg-slate-600 hover:bg-slate-500 text-white p-2 rounded justify-self-start'
+            >
+              Select Power Supply
+            </Link>
+          )}
         </div>
       </section>
     </main>
