0b686197628f17cb4cd832cbbb3219de4c6c7cf3
Mario Gilezan <mariogilezan10@gmail.com>
2023-05-31 17:38:45 +0200

    test link to storage

--- app/storage/page.js
+++ app/storage/page.js
@@ -7,16 +7,17 @@ export const metadata = {
   },
 };
 
-export default async function StoragePage() {
-  const req = await fetch('http://127.0.0.1:3000/api/componentsList');
-  const data = await req.json();
+export default function StoragePage() {
+  // const req = await fetch('http://127.0.0.1:3000/api/componentsList');
+  // const data = await req.json();
   return (
     <>
-      <ComponentsLayout
+      {/* <ComponentsLayout
         componentList={data.storage}
         componentName='storage'
         title='Storage'
-      />
+      /> */}
+      <h1>Storage</h1>
     </>
   );
 }
