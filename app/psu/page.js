46105716cf285c4483970d1812957c5a87f6bc74
Mario Gilezan <mariogilezan10@gmail.com>
2023-05-31 21:00:13 +0200

    add force cache

--- app/psu/page.js
+++ app/psu/page.js
@@ -8,7 +8,9 @@ export const metadata = {
 };
 
 export default async function PsuPage() {
-  const res = await fetch('http://127.0.0.1:3000/api/componentsList');
+  const res = await fetch('http://127.0.0.1:3000/api/componentsList', {
+    cache: 'force-cache',
+  });
   const data = await res.json();
   return (
     <>
